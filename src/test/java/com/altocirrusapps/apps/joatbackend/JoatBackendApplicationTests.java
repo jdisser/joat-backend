de51ee03da091fc2201f2f98a9b9f7f4c8f22d58
jrdisser <jimdisser@altocirrusapps.com>
2018-11-20 21:08:40 -0700

    inital commit from spring initialzr

---
+++ src/test/java/com/altocirrusapps/apps/joatbackend/JoatBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.altocirrusapps.apps.joatbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoatBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
