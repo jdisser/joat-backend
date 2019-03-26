1bf9d06c73e06f7e8f41ad4f151682020790dc40
jrdisser <jimdisser@altocirrusapps.com>
2019-03-26 16:16:06 -0600

    added Property repository

---
+++ src/main/java/com/altocirrusapps/apps/joatbackend/repositories/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.altocirrusapps.apps.joatbackend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altocirrusapps.apps.joatbackend.entities.Property;
+
+public interface PropertyRepository extends CrudRepository<Property, Long> {
+
+}
