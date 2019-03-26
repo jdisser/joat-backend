d656dd01b68645ea407c158e7bf5087aa253fa4b
jrdisser <jimdisser@altocirrusapps.com>
2019-03-26 16:40:33 -0600

    fixed PropertyRepository annotation

--- src/main/java/com/altocirrusapps/apps/joatbackend/repositories/PropertyRepository.java
+++ src/main/java/com/altocirrusapps/apps/joatbackend/repositories/PropertyRepository.java
@@ -1,9 +1,11 @@
 package com.altocirrusapps.apps.joatbackend.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.altocirrusapps.apps.joatbackend.entities.Property;
 
+@Repository
 public interface PropertyRepository extends CrudRepository<Property, Long> {
 
 }
